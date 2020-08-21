2e041c0994ea47a9990cc4d11d2838d01286ea5c
Akhil <mirya003@umn.edu>
2020-08-21 13:15:45 -0500

    added app.py file to phrasehunter folder

---
+++ phrasehunter/app.py
@@ -0,0 +1,16 @@
+# Import your Game class
+
+# Create your Dunder Main statement.
+
+# Inside Dunder Main:
+## Create an instance of your Game class
+## Start your game by calling the instance method that starts the game loop
+from game import Game
+from phrase import Phrase
+
+if __name__ == "__main__":
+    phrase = Phrase()
+    game = Game()
+
+print(phrase)
+print(game)
